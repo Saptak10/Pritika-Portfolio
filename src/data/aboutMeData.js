e6c4ced5c4dbd0cd83ca9120cd13386276ca0c95
Saptak10 <saptak.bugatti20@gmail.com>
2024-04-18 19:20:52 +0530

    first commit

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,18 @@
+export const aboutMeData = [
+  {
+    id: 1,
+    bio: "Hey there, I am Pritika Chatterjee, currently pursuing Ms.C. in Biotechnology from SRM University.",
+  },
+  {
+    id: 2,
+    bio: "Learning web development in mern stack.",
+  },
+  {
+    id: 3,
+    bio: "Email : pritikam2000@gmail.com",
+  },
+  {
+    id: 4,
+    bio: "Located at : Chennai, Tamil Nadu, India",
+  },
+];
